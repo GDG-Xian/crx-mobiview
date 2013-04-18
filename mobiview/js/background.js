b085a9861ed4cc1cf2e0ab9074aadea5c78e93c7
greatghoul <greatghoul@gmail.com>
2013-04-18 22:06:19 +0800

    Update view using current user-agent

--- mobiview/js/background.js
+++ mobiview/js/background.js
@@ -1,5 +1,4 @@
 var MOBIVIEW_URL = chrome.extension.getURL('mobiview.html');
-var IPHONE4 = 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_2 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8H7 Safari/6533.18.5';
 var HANDLERS = {};
 
 function storage(key, value) {
@@ -37,7 +36,8 @@ chrome.browserAction.onClicked.addListener(function(tab) {
                 function(details) {
                     for (var i = 0; i < details.requestHeaders.length; ++i) {
                         if (details.requestHeaders[i].name === 'User-Agent') {
-                            details.requestHeaders[i].value = IPHONE4;
+                            // FIXME: Move constants in separated file for global references.
+                            details.requestHeaders[i].value = storage('options-user-agent-value');
                         }
                         if (details.requestHeaders[i].name === 'X-Frame-Options') {
                             details.requestHeaders[i].value = "GOFORIT";
